e82be655f908c0ca4751f1e126faaaeddb5aa0d2
1030038593@qq.com <1030038593@qq.com>
2020-07-09 09:41:07 +0800

    递归求和

--- recursive_method.py
+++ recursive_method.py
@@ -5,14 +5,20 @@
   递归/栈的调用(栈是先进后出)
 """
 
+
 def fact(x):
     if x == 1:
         return 1
     else:
-        return x * fact(x-1)
+        return x * fact(x - 1)
+
+
+def sum(arr):
+    if len(arr) == 0:
+        return 0
+    return arr[0] + sum(arr[1:])
 
 
 if __name__ == '__main__':
     res = fact(5)
     print(res)
-
